27bcbc4f2d4f1c0855d274879b8a248586f0c8fd
gatewayitconsultingDesktop <43389272+gatewayitconsulting@users.noreply.github.com>
2021-09-11 16:47:31 -0500

    Removed images

--- src/views/SectionsPage/Sections/SectionWeddingParty.js
+++ src/views/SectionsPage/Sections/SectionWeddingParty.js
@@ -9,6 +9,10 @@ import GridItem from "components/Grid/GridItem.js";
 import NavPills from "components/NavPills/NavPills.js";
 import pillsStyle from "assets/jss/material-kit-pro-react/views/componentsSections/pillsStyle.js";
 
+import stevenImage from "assets/img/faces/avatar.jpg";
+import cameronImage from "assets/img/faces/cameron.jpg";
+import benImage from "assets/img/faces/ben.jpg";
+import michaelImage from "assets/img/faces/avatar.jpg";
 import image from "assets/img/faces/avatar.jpg";
 
 const useStyles = makeStyles(pillsStyle);
@@ -20,14 +24,14 @@ export default function SectionWeddingParty() {
       <div className={classes.container}>
         <div id="navigation-pills">
           <GridContainer>
-          <GridItem
+            <GridItem
               xs={12}
               className={
                 classes.mlAuto + " " + classes.mrAuto + " " + classes.textCenter
               }
             >
               <h2 className={classes.title + " " + classes.customTitle}>
-                  The Wedding Party
+                The Wedding Party
               </h2>
               <NavPills
                 color="dark"
@@ -39,35 +43,35 @@ export default function SectionWeddingParty() {
                       <GridContainer>
                         <GridItem xs={12} sm={3}>
                           <img
-                            src={image}
-                            alt="..."
-                            className={classes.imgRounded + " " + classes.imgFluid}
+                            src={stevenImage}
+                            alt="Steven Chaffin"
+                            className={classes.imgRoundedCircle + " " + classes.imgFluid}
                           />
-                          <h4 className={classes.imageTitle}>Rounded Image</h4>
+                          <h4 className={classes.imageTitle}>Steven Chaffin</h4>
                         </GridItem>
                         <GridItem xs={12} sm={3}>
                           <img
-                            src={image}
-                            alt="..."
-                            className={classes.imgRounded + " " + classes.imgFluid}
+                            src={cameronImage}
+                            alt="Cameron Cruz"
+                            className={classes.imgRoundedCircle + " " + classes.imgFluid}
                           />
-                          <h4 className={classes.imageTitle}>Rounded Image</h4>
+                          <h4 className={classes.imageTitle}>Cameron Cruz</h4>
                         </GridItem>
                         <GridItem xs={12} sm={3}>
                           <img
-                            src={image}
-                            alt="..."
-                            className={classes.imgRounded + " " + classes.imgFluid}
+                            src={benImage}
+                            alt="Ben Suntrup"
+                            className={classes.imgRoundedCircle + " " + classes.imgFluid}
                           />
-                          <h4 className={classes.imageTitle}>Rounded Image</h4>
+                          <h4 className={classes.imageTitle}>Ben Suntrup</h4>
                         </GridItem>
                         <GridItem xs={12} sm={3}>
                           <img
-                            src={image}
-                            alt="..."
-                            className={classes.imgRounded + " " + classes.imgFluid}
+                            src={michaelImage}
+                            alt="Michael Schisla"
+                            className={classes.imgRoundedCircle + " " + classes.imgFluid}
                           />
-                          <h4 className={classes.imageTitle}>Rounded Image</h4>
+                          <h4 className={classes.imageTitle}>Michael Schisla</h4>
                         </GridItem>
                       </GridContainer>
                     )
